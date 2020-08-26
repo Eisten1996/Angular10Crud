fb8273011a6ab073b8558b93cf01a19fa1081a7c
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-26 12:59:18 -0500

    feat: correciones de proxy

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const baseUrl = '/api/tutorials';
+const baseUrl = 'api/tutorials';
 @Injectable({
   providedIn: 'root',
 })
